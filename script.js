49f65ff84733340568528c49dc375b206ef959ea
Maria Eugenia O <eugeniaortiz162@gmail.com>
2023-06-29 20:16:09 -0300

    Funcionalidad: agregar imagen y texto

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// Ingreso de la imagen
 
 const inputImagen = document.getElementById('url-imagen')
 const imagenElegida = document.getElementById('imagen-meme')
@@ -10,3 +11,24 @@ const captarUrl = (e) => {
 
 inputImagen.addEventListener('input', (evento) => captarUrl(evento))
 
+
+// Ingreso del texto superior
+
+const textAreaSuperior = document.getElementById('texto-superior')
+const topTextMeme = document.getElementById('top-text')
+
+const editarTexto = (e) =>{
+    return topTextMeme.textContent = e.target.value;
+}
+textAreaSuperior.addEventListener('input', (evento) => editarTexto(evento))
+
+
+// Ingreso texto inferior
+
+const textAreaInferior = document.getElementById('texto-inferior')
+const bottomTextMeme = document.getElementById('bottom-text')
+
+const editarTexto2 = (e) =>{
+    return bottomTextMeme.textContent = e.target.value;
+}
+textAreaInferior.addEventListener('input', (evento) => editarTexto2(evento))
